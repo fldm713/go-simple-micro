116c12625835d87ab7ee8723efa83cf2f94a6768
fldm713 <ma.honglin95@gmail.com>
2022-08-15 15:30:14 -0700

    Add listener microservice with rabbitmq

---
+++ listener-service/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"listener/event"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ")
+
+	// start listening to messages
+	log.Println("Listening for and consuming rabbitmq messages")
+
+	// create consumer
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
+
+	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until we have a connection
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ is not ready yet, retrying in 1 second...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off for", backoff)
+		time.Sleep(backoff)
+		continue
+	}
+
+	return connection, nil
+}
